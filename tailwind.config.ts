ab7462be1fbd7b21b6075413348b0c101a78b4cd
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-06-14 17:27:42 +0200

    Colorainas

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,27 @@
+// tailwind.config.js
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lav': {
+          '50': '#fef1fa',
+          '100': '#fee5f8',
+          '200': '#feccf1',
+          '300': '#ff94e2',
+          '400': '#fe68d2',
+          '500': '#f83cbc',
+          '600': '#e81a9c',
+          '700': '#ca0c7e',
+          '800': '#a70d68',
+          '900': '#8b1058',
+          '950': '#550232',
+        },
+      },
+    },
   },
   plugins: [],
 }
